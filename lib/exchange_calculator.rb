702f66fc82f532d96f46e9630ca7866baa05e59e
David Rawson <dm.rawson@gmail.com>
2021-03-26 20:26:26 +0000

    Exchange calculator started. Incomplete.

---
+++ lib/exchange_calculator.rb
@@ -0,0 +1,14 @@
+require ReaderFactory
+
+class ExchangeCalculator
+
+    def intialize (file_path, home_currency: "EUR")
+        @file_path = file_path
+        @home_currency = home_currency
+    end
+
+    def calculate(date, from_currency, to_currency)
+
+    end
+
+end
