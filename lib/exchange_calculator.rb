7bee16635e1aaaaa78eb5bebbe3bde8a1e295184
David Rawson <dm.rawson@gmail.com>
2021-03-30 13:09:40 +0100

    More tests added. Currency validation strengthened

--- lib/exchange_calculator.rb
+++ lib/exchange_calculator.rb
@@ -1,27 +1,28 @@
-require_relative "reader_factory"
 require "date"
 
 class ExchangeCalculator
 
-  attr_reader :file_path, :base_currency, :rates
+  attr_reader :base_currency, :rates
 
-  def initialize(file_path, base_currency)
-    @file_path = file_path
+  def initialize(base_currency, rates)
     @base_currency = base_currency
-    @rates = ReaderFactory.new.create_reader(@file_path).read
+    @rates = rates
   end
 
   def calculate(date, from_currency, to_currency)
     date_string = date.strftime("%Y-%m-%d")
-    validate_currencies(from_currency, to_currency)
+    validate_currencies(from_currency, to_currency, date_string)
     validate_date(date_string)
 
     return base_currency_to_to_currency(date_string, to_currency).to_f if from_currency == @base_currency
     return from_currency_to_base_currency(date_string, from_currency).to_f if to_currency == @base_currency
   end
 
-  def validate_currencies(from_currency, to_currency)
-    raise "One of the currencies is not currently supported" if from_currency != @base_currency && to_currency != @base_currency
+  def validate_currencies(from_currency, to_currency, date_string)
+    currencies = @rates[date_string]
+
+    raise "#{from_currency} is not currently supported" unless currencies.key?(from_currency) || from_currency == @base_currency
+    raise "#{to_currency} is not currently supported" unless currencies.key?(to_currency) || to_currency == @base_currency
   end
 
   def validate_date(date_string)
@@ -29,14 +30,14 @@ class ExchangeCalculator
   end
 
   def base_currency_to_to_currency(date, to_currency)
-    get_rate(date, to_currency)
+    @rates[date][to_currency]
   end
 
   def from_currency_to_base_currency(date, from_currency)
-    1 / get_rate(date, from_currency)
+    1 / @rates[date][from_currency]
   end
 
-  def get_rate(date, currency)
-    @rates[date][currency]
+  def from_currency_to_to_currency(date, from_currency, to_currency)
+    @rates[date][to_currency] / @rates[date][from_currency]
   end
 end
--- test/currency_exchange_test.rb
+++ test/currency_exchange_test.rb
@@ -6,18 +6,65 @@
 require 'test/unit'
 require 'currency_exchange'
 require 'date'
+require 'exchange_calculator'
+require 'reader_factory'
 
 class CurrencyExchangeTest < Test::Unit::TestCase
   def setup
+    @base_currency = "EUR"
+    @file_path = "./data/eurofxref-hist-90d.json"
+    @rates = ReaderFactory.new.create_reader(@file_path).read
+
+    @test_rate = {"2021-02-28" => {"HUF" => 0.321, "ZAR" => 3.14159}}
+    @invalid_file = "./data/eurofxref-hist-90d.yoga"
   end
 
-  # def test_non_base_currency_exchange_is_correct
-  #   correct_rate = 1.2870493690602498
-  #   assert_equal correct_rate, CurrencyExchange.rate(Date.new(2018,11,22), "GBP", "USD")
-  # end
-
-  def test_base_currency_exchange_is_correct
+  def test_calculate_to_base_currency
     correct_rate = 0.007763975155279502
     assert_equal correct_rate, CurrencyExchange.rate(Date.new(2018,11,22), "JPY", "EUR")
+  end
+
+  def test_calculate_from_base_currency
+    correct_rate = 1290.52
+    assert_equal correct_rate, CurrencyExchange.rate(Date.new(2018,10,02), "EUR", "KRW")
+  end
+
+  def test_calculate_from_and_to_currencies
+    fixture = ExchangeCalculator.new(@base_currency, @test_rate)
+
+    correct_rate = 9.786884735202491
+    assert_equal correct_rate, fixture.from_currency_to_to_currency("2021-02-28", "HUF", "ZAR")
+  end
+
+  def test_valid_date_accepted
+    fixture = ExchangeCalculator.new(@base_currency, @rates)
+    assert_nil fixture.validate_date("2018-09-27")
+  end
+
+  def test_valid_currencies_accepted
+    fixture = ExchangeCalculator.new(@base_currency, @rates)
+    assert_nil fixture.validate_currencies("USD", "EUR", "2018-09-27")
+  end
+
+  # Exceptions
+  def test_raise_exception_when_no_rates_for_date
+    fixture = ExchangeCalculator.new(@base_currency, @rates)
+    exception = assert_raise(RuntimeError) {fixture.validate_date("2018-09-29")}
+    assert_equal "No exchange rates found for the given date", exception.message
+  end
+
+  def test_raise_exception_when_currency_not_supported
+    fixture = ExchangeCalculator.new(@base_currency, @rates)
+    exception = assert_raise(RuntimeError) {fixture.validate_currencies("YYY", "EUR", "2018-09-27")}
+    assert_equal "YYY is not currently supported", exception.message
+
+    exception = assert_raise(RuntimeError) {fixture.validate_currencies("EUR", "ZZZ", "2018-09-27")}
+    assert_equal "ZZZ is not currently supported", exception.message
+  end
+
+  def test_raise_exception_when_file_extension_not_supported
+    fixture = ReaderFactory.new
+    exception = assert_raise(RuntimeError) { fixture.create_reader(@invalid_file) }
+    assert_equal "File with extension .yoga does not have a corresponding Reader.", exception.message
   end
 end
